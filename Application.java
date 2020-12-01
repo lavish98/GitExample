0435eb72f260057c137b4dcce3442d23c6614abb
lavish98 <lavishchhabra21@gmail.com>
2020-12-01 15:05:35 +0530

    second commit

---
+++ Application.java
@@ -0,0 +1,13 @@
+package com.training;
+
+public class Application {
+
+ public static void main(String[] args) {
+
+	Greeting grtObj= new Greeting();
+
+	System.out.println(grtObj.getMessage());
+
+}
+
+}
